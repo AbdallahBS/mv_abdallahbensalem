cbe933745b9734e16a56b2b2c21d25b13cab485f
Abdallah Ben Salem <abenssalem32@gmail.com>
2024-05-06 11:37:55 +0100

    Doctrine Extensions: Timestampable

--- migrations/Version20240415103544.php
+++ migrations/Version20240415103544.php
@@ -20,12 +20,14 @@ final class Version20240415103544 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE vinyl_mix (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, track_count INT NOT NULL, genre VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE vinyl_mix ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE vinyl_mix ADD created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "Creation timestamp"');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE vinyl_mix');
+        $this->addSql('ALTER TABLE vinyl_mix  DROP updated_at, CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
+
 }
